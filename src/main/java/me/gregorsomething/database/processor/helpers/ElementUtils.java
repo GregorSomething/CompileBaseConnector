99e8412b86b29cbf1578dc157a765979b9288f55
GregorSomething <55884713+GregorSomething@users.noreply.github.com>
2025-04-04 21:23:05 +0300

    Fix style and remove dead code

--- src/main/java/me/gregorsomething/database/processor/helpers/ElementUtils.java
+++ src/main/java/me/gregorsomething/database/processor/helpers/ElementUtils.java
@@ -4,12 +4,12 @@ import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 import me.gregorsomething.database.processor.ProcessingValidationException;
 
-import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.Modifier;
-import javax.lang.model.element.VariableElement;
+import javax.lang.model.element.*;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class ElementUtils {
 
@@ -41,5 +41,37 @@ public final class ElementUtils {
             return types.getFirst();
         }
         throw new ProcessingValidationException("Expected DeclaredType element!", element);
+    }
+
+    /**
+     * Maps type generics to their actual types, where the key is a generic {@link javax.lang.model.type.TypeMirror}
+     * (e.g., {@code TypeMirror{type='T'}}), and the value is its corresponding resolved type from {@code returnType}.
+     *
+     * <p><b>Note:</b> {@code returnType} must not be a generic {@link javax.lang.model.type.TypeMirror}.
+     * Converting a {@link javax.lang.model.type.TypeMirror} to a {@link javax.lang.model.element.TypeElement} will
+     * lose implementation-specific type information. For example, generic class parameters (like {@code K}, {@code V})
+     * will be used instead type variables, even when converted back to {@link javax.lang.model.type.TypeMirror}.</p>
+     *
+     * @param returnType the type whose generic parameters are to be mapped.
+     * @return a {@link java.util.Map} mapping each generic {@link javax.lang.model.type.TypeMirror} to its corresponding
+     *         concrete {@link javax.lang.model.type.TypeMirror}. Returns an empty map if {@code returnType}
+     *         is not a {@link javax.lang.model.type.DeclaredType}.
+     */
+    public static Map<TypeMirror, TypeMirror> makeParamTypeMap(TypeMirror returnType) {
+        if (returnType instanceof DeclaredType type
+                && type.asElement() instanceof TypeElement element) {
+            Map<TypeMirror, TypeMirror> typesMapping = new HashMap<>();
+            List<? extends TypeParameterElement> typeParameters = element.getTypeParameters();
+            List<? extends TypeMirror> typeArguments = type.getTypeArguments();
+            if (typeParameters.size() != typeArguments.size())
+                throw new IllegalStateException("Type parameters and type arguments should be same size");
+            for (int i = 0; i < typeParameters.size(); i++) {
+                TypeMirror genericType = typeParameters.get(i).asType();
+                TypeMirror actualType = typeArguments.get(i);
+                typesMapping.put(genericType, actualType);
+            }
+            return typesMapping;
+        }
+        return Map.of();
     }
 }
--- src/main/java/me/gregorsomething/database/processor/types/ComplexTypeMapperGenerator.java
+++ src/main/java/me/gregorsomething/database/processor/types/ComplexTypeMapperGenerator.java
@@ -6,14 +6,15 @@ import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeName;
 import lombok.RequiredArgsConstructor;
 import me.gregorsomething.database.processor.RepositoryProcessor;
+import me.gregorsomething.database.processor.helpers.ElementUtils;
 import me.gregorsomething.database.processor.helpers.Pair;
 
-import javax.lang.model.element.*;
-import javax.lang.model.type.DeclaredType;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -31,12 +32,6 @@ public class ComplexTypeMapperGenerator {
                                                            String methodName, ExecutableElement constructor) {
         MethodSpec.Builder method = this.generateMethodHeadFor(methodName, returnType);
         CodeBlock.Builder code = CodeBlock.builder();
-        if (returnType instanceof DeclaredType t) {
-            System.out.println("Tere");
-            TypeElement element = (TypeElement) t.asElement();
-            List<? extends TypeParameterElement> typeParameters = element.getTypeParameters();
-            System.out.println(element);
-        }
         this.readParametersToVariables(selectedItems, constructor.getParameters(), returnType, code);
         this.addConstructorCallAndReturn(code, returnType, constructor);
         method.addCode(code.build());
@@ -65,7 +60,7 @@ public class ComplexTypeMapperGenerator {
 
     private void readParametersToVariables(List<Pair<Integer, String>> selectedItems, List<? extends VariableElement> variableElements,
                                            TypeMirror returnType , CodeBlock.Builder code) {
-        Map<TypeMirror, TypeMirror> typeMap = this.makeParamTypeMap(returnType);
+        Map<TypeMirror, TypeMirror> typeMap = ElementUtils.makeParamTypeMap(returnType);
         for (VariableElement element : variableElements) {
             Pair<Integer, String> item = selectedItems.stream()
                     .filter(i -> i.right().equalsIgnoreCase(element.getSimpleName().toString()))
@@ -75,24 +70,6 @@ public class ComplexTypeMapperGenerator {
                     element.getSimpleName().toString(),
                     typeMap.getOrDefault(element.asType(), element.asType()));
         }
-    }
-
-    private Map<TypeMirror, TypeMirror> makeParamTypeMap(TypeMirror returnType) {
-        if (returnType instanceof DeclaredType type
-                && type.asElement() instanceof TypeElement element) {
-            Map<TypeMirror, TypeMirror> typesMapping = new HashMap<>();
-            List<? extends TypeParameterElement> typeParameters = element.getTypeParameters();
-            List<? extends TypeMirror> typeArguments = type.getTypeArguments();
-            if (typeParameters.size() != typeArguments.size())
-                throw new IllegalStateException("Type parameters and type arguments should be same size");
-            for (int i = 0; i < typeParameters.size(); i++) {
-                TypeMirror genericType = typeParameters.get(i).asType();
-                TypeMirror actualType = typeArguments.get(i);
-                typesMapping.put(genericType, actualType);
-            }
-            return typesMapping;
-        }
-        return Map.of();
     }
 
     private void readParameterVariable(CodeBlock.Builder code, int resultSetPos, String variableName, TypeMirror type) {
